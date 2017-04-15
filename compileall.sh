c3f0503d40da41d43b22588f6dc41e59aea384b2
lmhieu612 <hieule@hieules-MacBook-Pro.local>
2017-04-15 16:30:01 -0400

    update

---
+++ compileall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "HL dep trai qua anh oi"
+python jemdoc.py projects.jemdoc
+python jemdoc.py misc.jemdoc
+python jemdoc.py publications.jemdoc
+python jemdoc_mathjax.py -c mysite.conf index
