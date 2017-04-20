894f293fb7b69e3c9cca06011bd97b0c25299b46
lmhieu612 <hieule@hieules-MacBook-Pro.local>
2017-04-19 21:55:33 -0400

    project page

--- compileall.sh
+++ compileall.sh
@@ -3,4 +3,7 @@ echo "HL dep trai qua anh oi"
 python jemdoc.py projects.jemdoc
 python jemdoc.py misc.jemdoc
 python jemdoc.py publications.jemdoc
+python jemdoc.py lymeye.jemdoc
+
+python jemdoc.py depixel.jemdoc
 python jemdoc_mathjax.py -c mysite.conf index
