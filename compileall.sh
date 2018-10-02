870ff6f92b55d0670893ce1b16cef5d757c61f82
lmhieu612 <hieule@hieules-MacBook-Pro.local>
2018-10-02 08:33:44 +0700

    add

--- compileall.sh
+++ compileall.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "HL dep trai qua anh oi"
-python jemdoc.py projects.jemdoc
-python jemdoc.py misc.jemdoc
-python jemdoc.py publications.jemdoc
-python jemdoc.py lymeye.jemdoc
+/usr/bin/python2.7 jemdoc.py projects.jemdoc
+/usr/bin/python2.7 jemdoc.py misc.jemdoc
+/usr/bin/python2.7 jemdoc.py publications.jemdoc
+/usr/bin/python2.7 jemdoc.py lymeye.jemdoc
 
-python jemdoc.py depixel.jemdoc
-python jemdoc_mathjax.py -c mysite.conf index
+/usr/bin/python2.7 jemdoc.py depixel.jemdoc
+/usr/bin/python2.7 jemdoc_mathjax.py -c mysite.conf index
 git add *
 git commit -m 'add'
 git push origin master
