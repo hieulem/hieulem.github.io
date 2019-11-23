0095870de6c72560fdf88c461fe1f568796a9963
lmhieu612 <hieule@hieules-MacBook-Pro.local>
2019-11-23 17:00:55 -0500

    add

--- compileall.sh
+++ compileall.sh
@@ -5,5 +5,4 @@ echo "HL dep trai qua anh oi"
 /usr/bin/python2.7 jemdoc.py publications.jemdoc
 /usr/bin/python2.7 jemdoc.py lymeye.jemdoc
 
-/usr/bin/python2.7 jemdoc.py depixel.jemdoc
 /usr/bin/python2.7 jemdoc_mathjax.py -c mysite.conf index
